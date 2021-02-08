8f4eb33ac6ef115349546b0acc9312823d3924e8
mrdanishjr1992 <mrdanishjr1992@gmail.com>
2021-02-08 17:13:34 -0600

    Adds heroku deployment config

--- storagespace_project/settings.py
+++ storagespace_project/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
 from pathlib import Path
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +130,5 @@ LOGIN_REDIRECT_URL = 'explore'
 
 # LOGOUT_URL = '/'
 LOGOUT_REDIRECT_URL = 'login'
+
+django_on_heroku.settings(locals())
