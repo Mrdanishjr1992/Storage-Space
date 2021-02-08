16ecd88ffcf465e92a7fd1820b17f8331071bb4b
mrdanishjr1992 <mrdanishjr1992@gmail.com>
2021-02-08 03:14:25 -0600

    comments on explore page

--- main_app/urls/explore_urls.py
+++ main_app/urls/explore_urls.py
@@ -4,5 +4,6 @@ from django.urls import reverse
 
 urlpatterns = [
     # HOME-PAGE
-    path("", explore_views.explore, name="explore")
+    path("", explore_views.explore, name="explore"),
+    path("<int:collection_id>/", explore_views.explore_comment, name="explore_comment")
 ]
--- main_app/views/explore_views.py
+++ main_app/views/explore_views.py
@@ -3,12 +3,21 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.conf import settings
-from ..models import Collection
+from ..models import Collection, Comment
 from ..forms import CollectionForm
 
 def explore(request):
     collections = Collection.objects.filter(public = True)
     context = {
         'collections': collections,
+    }
+    return render(request,'public_collection/explore.html', context)
+
+def explore_comment(request, collection_id):
+    collections = Collection.objects.filter(public = True)
+    comments = Comment.objects.filter(collection_id=collection_id)
+    context = {
+        'collections': collections,
+        'comments': comments
     }
     return render(request,'public_collection/explore.html', context)
