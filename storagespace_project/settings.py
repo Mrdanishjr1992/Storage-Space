4899c0e900b65995b0c17dcdb06173ece1c04c0f
mrdanishjr1992 <mrdanishjr1992@gmail.com>
2021-02-08 22:54:55 -0600

    bug fix trial

--- storagespace_project/settings.py
+++ storagespace_project/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'storagespace_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR | 'storagespace'
+        'NAME': 'storagespace'
     }
 }
 
